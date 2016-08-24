afb583357795a3c4aba44f3e571a775444ecdc75
helloitslogan <logan.capuano@icloud.com>
2016-08-23 20:19:13 -0400

    Add files via upload

---
+++ CoreEssential/src/me/loganxdaniel/coreessentials/Main.java
@@ -0,0 +1,14 @@
+package me.loganxdaniel.coreessentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Logan on 8/23/16.
+ */
+public class Main extends JavaPlugin {
+
+    public void onEnable() {
+    }
+
+
+}
